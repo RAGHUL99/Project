e94d1525a833dd79bdc88d384ba21f05e6bc53ce
RAGHUL99 <raghulshankaran1999@gmail.com>
2021-06-16 22:30:24 +0530

    backend for designer done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
+var sql = require("mssql/msnodesqlv8");
 
 const app = express()
 
@@ -91,6 +92,66 @@ app.post("/Login", (req,res)=>{
 });
 
 
+app.post("/Designer", function (req, res){
+    // const dbconfig = {
+    //     user: "rahul",
+    //     password: "Test123",
+    //     server: "MSSQLSERVER",
+    //     database: "Back office operations",
+        
+
+    // };
+    var sqlConfig = {
+        user: 'rahul', //username created from SQL Management Studio
+        password: 'Test123',
+        server: 'LAPTOP-6A75V5HK',    //the IP of the machine where SQL Server runs
+        port:'1433',
+        driver: 'msnodesqlv8',
+        options: {
+            //instanceName: 'MSSQLSERVER',
+            database: "Back office operations",  //the username above should have granted permissions in order to access this DB.
+            debug: {
+                packet: false,
+                payload: false,
+                token: false,
+                data: false
+            },
+            //encrypt: true
+        }
+    
+    };
+    function des(){
+        var conn = new sql.ConnectionPool(sqlConfig);
+        var request = new sql.Request(conn);
+
+        conn.connect(function (err){
+            if (err){
+                console.log(err);
+                return;
+            }
+            request.query("Insert into Logistics (Order_ID, Design_Status, Invoice_Status, Bill_Status) VALUES ('"+req.body.Oid+"','"+req.body.DR+"','"+req.body.IS+"','"+req.body.BS+"')", function(err, recordset){
+                if (err){
+                    console.log(err);
+                }
+                else{
+                    console.log("1 Field Inserted");
+                    console.log(recordset);
+                    res.redirect("/design.html");
+                }
+                conn.close();
+            });
+        });
+    }
+    des()
+
+});
+  
+//   var server = app.listen(5000, function () {
+//       console.log('Server is listening at port 5000...');
+
+//   });
+
+
 app.get("/",(req,res)=>{
     res.set({
         "Allow-access-Allow-Origin": '*'
@@ -106,4 +167,7 @@ app.get("/Login",(req, res)=>{
     res.redirect("/Login.html");
 });
 
+app.get("/Designer",(req, res)=>{
+    res.redirect("/design.html");
+});
 console.log("Listening on PORT 3000");
